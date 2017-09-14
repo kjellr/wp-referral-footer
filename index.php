580ed86b6d71fc1acdf557ad0da7d3074c90ff16
Kjell Reigstad <kjell@kjellr.com>
2017-09-13 17:14:22 -0700

    Adding plugin headers.

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * WordPress.com Referral Footer
+ *
+ * @package     ReferralFooter
+ * @author      Kjell Reigstad
+ * @copyright   2017 Your Name or Company Name
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: WordPress.com Referral Footer
+ * Plugin URI:  http://kjellr.com
+ * Description: Displays a "Powered by WordPress" footer element with a referral footer.
+ * Version:     0.1
+ * Author:      Kjell Reigstad
+ * Author URI:  http://kjellr.com
+ * Text Domain: wp-referral-footer
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
