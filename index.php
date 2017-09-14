5bde2c22517c1f3cff318cfb8bb0e5a6baecbae2
Kjell Reigstad <kjell@kjellr.com>
2017-09-14 15:12:10 -0700

    Adding fields via the settings API

--- index.php
+++ index.php
@@ -20,7 +20,6 @@
  */
 
 class ReferralFooter {
-
 	
 	function __construct() {
 		define( 'REFERRALFOOTER__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
@@ -28,73 +27,16 @@ class ReferralFooter {
 		add_action( 'admin_menu', array( $this, 'wpcom_referral_footer_options' ) );
 		add_action( 'get_footer', array( $this, 'wpcom_referral_footer_render' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'wpcom_referral_footer_css' ) );
+
+		add_action( 'admin_init', array( $this, 'wpcom_referral_footer_settings_init' ) );
 	}
 
 	/**
 	 * Add the "Referral Footer" item to WP-Admin
 	 */
 	function wpcom_referral_footer_options() {
-		add_theme_page( 'WordPress.com Referral Footer Options', 'Referral Footer', 'manage_options', 'wpcom_referral_footer_options', array( $this, 'wpcom_referral_footer_options_content' ) );
+		add_theme_page( 'WordPress.com Referral Footer Options', 'Referral Footer', 'manage_options', 'wpcom_referral_footer_options', array( $this, 'wpcom_referral_footer_options_page' ) );
 	}
-
-	/**
-	 * Include the plugin settings page, but only if
-	 * the current user has the correct permissions.
-	 */
-	function wpcom_referral_footer_options_content() { ?>
-
-		<div class="wrap">
-			<h1 class="wp-heading-inline"><?php _e( 'WordPress.com Referral Footer' ); ?></h1>
-
-			<form action="#">
-				<br />
-				<h2 class="title"><?php _e( 'Setup' ); ?></h2>
-				<table class="form-table">
-					<tbody>
-						<tr class="form-field">
-							<th scope="row">
-								<label for="referral_url"><?php _e( 'Your Referral URL' ); ?></label>
-							</th>
-							<td>
-								<input name="referral_url" type="text" id="user_login" value="" style="width:25em;">
-							</td>
-						</tr>
-					</tbody>
-				</table>
-				<br /><br />
-				<h2 class="title"><?php _e( 'Style Customization' ); ?></h2>
-				<table class="form-table">
-					<tbody>
-						<tr class="form-field">
-							<th scope="row">
-								<label for="logo_color"><?php _e( 'Logo Color' ); ?></label>
-							</th>
-							<td>
-								<input name="logo_color" type="text" id="user_login" value="" style="width:10em;" maxlength="7">
-							</td>
-						</tr>
-						<tr class="form-field">
-							<th scope="row">
-								<label for="text_color"><?php _e( 'Text Color' ); ?></label>
-							</th>
-							<td>
-								<input name="text_color" type="text" id="user_login" value="" style="width:10em;" maxlength="7">
-							</td>
-						</tr>
-						<tr class="form-field">
-							<th scope="row">
-								<label for="hover_color"><?php _e( 'Hover Color' ); ?></label>
-							</th>
-							<td>
-								<input name="hover_color" type="text" id="user_login" value="" style="width:10em;" maxlength="7">
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</form>
-		</div>
-			
-	<?php }
 
 	/**
 	 * Include the footer element itself.
@@ -108,6 +50,123 @@ class ReferralFooter {
 	 */
 	function wpcom_referral_footer_css() {
 		wp_enqueue_style(  'wpcom-referral-footer-styles', plugins_url( 'style.css', __FILE__ ) );
+	}
+
+	/**
+	 * Register Plugin Settings
+	 */
+	function wpcom_referral_footer_settings_init() { 
+
+		register_setting( 'ReferralFooterPage', 'wpcom_referral_footer_settings' );
+
+		add_settings_section(
+			'wpcom_referral_footer_ReferralFooterPage_setup', 
+			__( 'Setup', 'wpcom-referral-footer' ), 
+			array( $this, 'wpcom_referral_footer_settings_setup_callback' ), 
+			'ReferralFooterPage'
+		);
+
+		add_settings_section(
+			'wpcom_referral_footer_ReferralFooterPage_styles', 
+			__( 'Styles', 'wpcom-referral-footer' ), 
+			array( $this, 'wpcom_referral_footer_settings_styles_callback' ), 
+			'ReferralFooterPage'
+		);
+
+		add_settings_field( 
+			'wpcom_referral_footer_field_refer_url', 
+			__( 'Referral URL', 'wpcom-referral-footer' ), 
+			array( $this, 'wpcom_referral_footer_field_refer_url_render' ), 
+			'ReferralFooterPage', 
+			'wpcom_referral_footer_ReferralFooterPage_setup' 
+		);
+
+		add_settings_field( 
+			'wpcom_referral_footer_field_logo_color', 
+			__( 'Logo Color', 'wpcom-referral-footer' ), 
+			array( $this, 'wpcom_referral_footer_field_logo_color_render' ), 
+			'ReferralFooterPage', 
+			'wpcom_referral_footer_ReferralFooterPage_styles' 
+		);
+
+		add_settings_field( 
+			'wpcom_referral_footer_field_text_color', 
+			__( 'Text Color', 'wpcom-referral-footer' ), 
+			array( $this, 'wpcom_referral_footer_field_text_color_render' ), 
+			'ReferralFooterPage', 
+			'wpcom_referral_footer_ReferralFooterPage_styles' 
+		);
+
+		add_settings_field( 
+			'wpcom_referral_footer_field_hover_color', 
+			__( 'Text Hover Color', 'wpcom-referral-footer' ), 
+			array( $this, 'wpcom_referral_footer_field_hover_color_render' ), 
+			'ReferralFooterPage', 
+			'wpcom_referral_footer_ReferralFooterPage_styles' 
+		);
+
+
+	}
+
+
+	/**
+	 * Settings Fields
+	 */
+	function wpcom_referral_footer_field_refer_url_render() {
+		$options = get_option( 'wpcom_referral_footer_settings' );
+		?>
+		<input type='text' name='wpcom_referral_footer_settings[wpcom_referral_footer_field_refer_url]' value='<?php echo $options['wpcom_referral_footer_field_refer_url']; ?>'>
+		<?php
+	}
+
+	function wpcom_referral_footer_field_logo_color_render() {
+		$options = get_option( 'wpcom_referral_footer_settings' );
+		?>
+		<input type='text' name='wpcom_referral_footer_settings[wpcom_referral_footer_field_logo_color]' value='<?php echo $options['wpcom_referral_footer_field_logo_color']; ?>' maxlength="6">
+		<?php
+	}
+
+	function wpcom_referral_footer_field_text_color_render() {
+		$options = get_option( 'wpcom_referral_footer_settings' );
+		?>
+		<input type='text' name='wpcom_referral_footer_settings[wpcom_referral_footer_field_text_color]' value='<?php echo $options['wpcom_referral_footer_field_text_color']; ?>' maxlength="6">
+		<?php
+	}
+
+	function wpcom_referral_footer_field_hover_color_render() {
+		$options = get_option( 'wpcom_referral_footer_settings' );
+		?>
+		<input type='text' name='wpcom_referral_footer_settings[wpcom_referral_footer_field_hover_color]' value='<?php echo $options['wpcom_referral_footer_field_hover_color']; ?>' maxlength="6">
+		<?php
+	}
+
+	/**
+	 * Callback functions for each section
+	 */
+	function wpcom_referral_footer_settings_setup_callback() {
+		echo __( 'Insert your referral link.', 'wpcom-referral-footer' );
+	}
+	function wpcom_referral_footer_settings_styles_callback() {
+		echo __( 'Customize the footer.', 'wpcom-referral-footer' );
+	}
+
+	/**
+	 * Render the options page
+	 */
+	function wpcom_referral_footer_options_page() {
+		?>
+		<form action='options.php' method='post'>
+
+			<h2>WordPress.com Referral Footer</h2>
+
+			<?php
+			settings_fields( 'ReferralFooterPage' );
+			do_settings_sections( 'ReferralFooterPage' );
+			submit_button();
+			?>
+
+		</form>
+		<?php
 	}
 
 }
